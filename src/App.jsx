6efd28ee00ffe6830efcecc26afd0e89db08ad98
Asif saif ali <asif263848@gmail.com>
2024-01-10 20:30:57 +0530

    Added an object to add dummy data

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,8 @@
 import './App.css'
-<<<<<<< HEAD
 import Form from './components/Form/Form'
 // import { FormPage } from './components/Page/FormPage';
 import FormPage from './components/Page/FormPage';
 import { BrowserRouter as Router, Routes, Route} from 'react-router-dom'
-=======
-// import Form from './components/Form/Form'
-import {FormPage} from './components/Page/FormPage';
->>>>>>> a71d57a2cbc4e154177ae04c039afc37baa35d44
 function App() {
 
  
@@ -16,9 +11,8 @@ function App() {
   // }
   return (
     <>
-<<<<<<< HEAD
-    {/* //    <Form />
-    //   <button onClick={handlePrint}>🖨️</button>  */}
+       <Form /> 
+      <button onClick={handlePrint}>🖨️</button> 
      
      <Router>
       <Routes>
@@ -26,11 +20,6 @@ function App() {
         <Route path="/formPage" element={<Form />} />
       </Routes>
     </Router>
-=======
-       {/* <Form />
-      <button onClick={handlePrint}>🖨️</button> */}
-      <FormPage/>
->>>>>>> a71d57a2cbc4e154177ae04c039afc37baa35d44
     </>
   )
 }
